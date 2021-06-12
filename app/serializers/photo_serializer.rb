3ae32af0bf49547ca558b2e78e4b88f5a5d5c901
yulia <juliana.ny2008@gmail.com>
2021-06-12 13:29:06 -0400

    changing image url

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,14 +1,15 @@
 class PhotoSerializer < ActiveModel::Serializer
     attributes :id, :user_id, :category, :votes, :description, :attached_image
 
-    # def image 
-        # for url on AWS
-        # object.image.blob.key if object.image.attached?   
-    # end
-
     def attached_image 
+        # for url on AWS
+        # object.image.blob.key if object.image.attached?  
+        object.image_url if object.image.attached?   
+    end
+
+    #def attached_image 
         #for url on localhost
-        object.get_image_url if object.image.attached? 
-    end 
+        #object.get_image_url if object.image.attached? 
+    #end 
 end
   
